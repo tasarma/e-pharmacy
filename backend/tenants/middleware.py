98469ab5ba8ee8d9db4fcf32359c4a42a6b79055
tasarma <tasarm7@protonmail.com>
2025-07-31 10:38:05 +0200

    updated middleware to bypass tenant enforcment for admin

--- backend/tenants/middleware.py
+++ backend/tenants/middleware.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from typing import Optional, Callable
 
 from .models import Tenant
-from .context import set_tenant_context
+from .context import set_tenant_context, tenant_context_disabled
 
 
 class TenantAwareMiddleware:
@@ -15,21 +15,24 @@ class TenantAwareMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
-        # TODO: IN PRODUCTION use host = request.get_host() which is more safer
+        # TODO: Activate in production
+        # if not request.user.is_authenticated:
+        #     return self.get_response(request)
+
+        if request.path.startswith("/admin/"):
+            with tenant_context_disabled():
+                return self.get_response(request)
+
+        # TODO: IN PRODUCTION use host = request.get_host()
         host = request.META.get("HTTP_HOST", "")
         subdomain = self.get_subdomain(host)
+        tenant = self.get_tenant(subdomain) if subdomain else None
 
-        if subdomain is None:
-            return self.get_response(request)
-
-        try:
-            tenant = Tenant.objects.get(subdomain=subdomain, active=True)
-        except ObjectDoesNotExist:
+        if tenant is None:
             raise Http404(f"Tenant with subdomain '{subdomain}' not found")
 
         with set_tenant_context(tenant=tenant):
             response = self.get_response(request)
-
         return response
 
     def get_subdomain(self, host: str) -> Optional[str]:
@@ -39,11 +42,20 @@ class TenantAwareMiddleware:
         if not host:
             return None
 
-        # Remove port (e.g., 'example.com:8000' -> 'example.com')
+        # Remove port ('example.com:8000' -> 'example.com')
         host = host.split(":")[0]
         parts = host.split(".")
 
-        if len(parts) < 3:
-            return None
+        return parts[0] if len(parts) >= 3 else None
 
-        return parts[0]
+    def get_tenant(self, subdomain: str) -> Optional[Tenant]:
+        """
+        Get Tenant using subdomain.
+        """
+        # TODO:  Cashe tenant lookup
+        # TODO:  Get tenant from session for logged in users and from request for anonymous users
+
+        try:
+            return Tenant.objects.get(subdomain=subdomain, active=True)
+        except ObjectDoesNotExist:
+            return None
