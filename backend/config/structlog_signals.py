d564810056c1e4074923ca8f35a7f33c626e8a8e
tasarma <tasarm7@protonmail.com>
2025-10-09 16:10:17 +0200

    added tenant and subdoamin metadata to log message

--- backend/config/structlog_signals.py
+++ backend/config/structlog_signals.py
@@ -3,8 +3,19 @@ from django.dispatch import receiver
 from django_structlog.signals import bind_extra_request_metadata
 import structlog
 
+from tenants.context import get_current_tenant
 
 @receiver(bind_extra_request_metadata)
 def remove_ip_address(request, logger, **kwargs):
     structlog.contextvars.bind_contextvars(ip=None)
+
+@receiver(bind_extra_request_metadata)
+def remove_request_id(request, logger, **kwargs):
+    structlog.contextvars.bind_contextvars(request_id=None)
+
+@receiver(bind_extra_request_metadata)
+def bind_tenant_and_subdomain(request, logger, **kwargs):
+    current_tenant = get_current_tenant()
+    structlog.contextvars.bind_contextvars(tenant=current_tenant.name)
+    structlog.contextvars.bind_contextvars(subdomain=current_tenant.subdomain)
 
