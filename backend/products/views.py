e97f5c40d40ec0a8eea5c20e5e91517e6255fb63
Mahmut Tasar <tasarm7@protonmail.com>
2026-01-14 15:41:59 +0100

    Frontend (#30)
    * created frontend folder with docs/ARCHITECTURE.md file
    
    * initial setup
    
    * added .env to .gitignore
    
    * updated .gitignore files
    
    * established the connection with backend
    
    * established the connection with frontend and added fromating

--- backend/products/views.py
+++ backend/products/views.py
@@ -23,9 +23,6 @@ from .permissions import IsStaffOrReadOnly, IsTenantUser
 from tenants.context import get_current_tenant
 
 
-from tenants.context import get_current_tenant
-
-
 logger = structlog.get_logger(__name__)
 
 
