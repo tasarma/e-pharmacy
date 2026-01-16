ad9570100b5e55b9d0685ed8ac6c295ab2b19c93
Mahmut Tasar <tasarm7@protonmail.com>
2026-01-16 13:49:34 +0100

    Dev (#34)
    * removed .env from git
    
    * added  .agent to .gitignore
    
    * Move all backend code into a src folder
    
    * updated Python version to 3.14
    
    * dockerized the backend
    
    * Switch to Gunicorn and Whitenoise

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -138,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # For production - use cloud storage
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
