05b10eb4ae048a00dbd43911cbfa7484530f5c37
tasarma <tasarm7@protonmail.com>
2026-01-27 13:14:59 +0100

    created variables, outputs with env  variables for terraform

--- backend/infrastructure/terraform/main.tf
+++ backend/infrastructure/terraform/main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = "eu-central-1"
+  region  = var.aws_region
+  profile = "dene"
 }
 
 data "aws_ami" "ubuntu" {
@@ -12,13 +13,55 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-/*
-resource "aws_instance" "backend" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+
+# Security Group: defines who can enter and leave your server
+resource "aws_security_group" "app_sg" {
+  name = "app-security-group"
+
+  # This allows Ansible (and you) to log into the server to install Docker and manage apps
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  # This allows the general public to view your website/app once it's running in Docker
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+  }
+
+  # This allows your server to "talk back" to the internet—crucial for downloading Docker images or security updates
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outband
+  }
 
   tags = {
-    Name = "terraform-test"
+    Name = "app-sg"
   }
 }
-*/
+
+resource "aws_instance" "backend" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  # Ensure a dynamic public IP is assigned
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ backend/infrastructure/terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "instance_name" {
+  description = "Value of the EC2 instance's Name tag."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The EC2 instance's type."
+  type        = string
+}
+
+variable "key_name" {
+ description = "SSH key pair name"
+ type        = string
+}
